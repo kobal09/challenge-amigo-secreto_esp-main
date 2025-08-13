51301eb50537310c9a80c553a511ed015cabf947
kobal09 <kobal0901@gmail.com>
2025-08-13 01:29:24 -0500

    crear la funcionalidad de la pagina

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = [];
+
+let $_txt = document.getElementById('amigo');
+let $_ulListaAmigos = document.getElementById('listaAmigos');
+let $_ulganador = document.getElementById('resultado');
+let $_liListaAmigos = "";
+let $_liganador = "";
+
+
+function agregarAmigo() {
+    let contenido = $_txt.value;
+
+    if (contenido === '') {
+        return alert('no puedes ingresar una caja vacia');
+    } else {
+        listaAmigos.push(contenido);
+    } 
+    mostrarAmigos(listaAmigos);            
+}
+
+function mostrarAmigos(listaAmigos) {
+    console.log(listaAmigos);
+    $_liListaAmigos = "";
+    for (let i = 0; i < listaAmigos.length; i++) {
+        $_liListaAmigos += `<li>${listaAmigos[i]}</li>`;        
+    }
+    $_ulListaAmigos.innerHTML = $_liListaAmigos;
+}
+
+function sortearAmigo(listaAmigos) {
+    //console.log(listaAmigos);
+    let g = '';
+    let indiceAmigoSecreto = Math.floor(Math.random() * listaAmigos.length);
+    g = listaAmigos[indiceAmigoSecreto];
+    console.log(g);
+    mostrarAmigoGanador(g);
+}
+
+function mostrarAmigoGanador(g) {
+    $_ulganador.innerHTML = g;
+}
+
+//console.log(listaAmigos);
